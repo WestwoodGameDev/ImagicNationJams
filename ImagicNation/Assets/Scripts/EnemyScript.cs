e34ea41c7e2443351ab0fdf40d3e83eeba56efab
jamjammers <jamesetam@gmail.com>
2023-10-11 14:44:11 -0500

    idrk XD
    fix somethingsm, oncol works, need to do scrol
    enemy btr?
    need to work on kinematics

--- ImagicNation/Assets/Scripts/EnemyScript.cs
+++ ImagicNation/Assets/Scripts/EnemyScript.cs
@@ -10,6 +10,7 @@ public class EnemyScript : MonoBehaviour
     public bool inRange = false;
     public float attack = 0;
     public float cd = 0;
+    public float atkVelo = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,18 +23,28 @@ public class EnemyScript : MonoBehaviour
 
         //slime script
         if(type == "slime"){
-            if(inRange&&cd<=0&&attack<0){
-                attack = 0;
-            }else if(inRange&&cd<=0){
-                attack += Time.deltaTime;
+            if(cd<=0){
+                if(inRange&&attack>=0.66){
+                    attack = 0;
+                }else if(inRange){
+                    attack += Time.deltaTime;
+                }
+                if(attack>=0.66){
+                    cd = 5;
+                    rb.velocity = new Vector2(atkVelo, rb.velocity.y);
+                }
+            }else{
+                cd -= Time.deltaTime;
             }
-            if(attack>=0.66){
-                cd = 5;
-                rb.velocity = new Vector2(2, rb.velocity.y);
-            }
+            
         }
-        if(rb.velocity.x)
-        rb.velocity = new Vector2(rb.velocity.x-0.1f, rb.velocity.y);
+        if(rb.velocity.x>0.1f){
+            rb.velocity = new Vector2(rb.velocity.x-10*Time.deltaTime, rb.velocity.y) ;
+        }else if(rb.velocity.x<-0.1f){
+            rb.velocity = new Vector2(rb.velocity.x+10*Time.deltaTime, rb.velocity.y);
+        }else if(rb.velocity.x != 0){
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
     }
     public void onTriggerEnter(Collider col){
     
