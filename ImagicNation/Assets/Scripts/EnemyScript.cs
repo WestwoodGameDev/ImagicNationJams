b0a4687cda5e97ef263e8162abb53652282bba3e
jamjammers <jamesetam@gmail.com>
2023-10-11 08:51:18 -0500

    tryed some things
    slime atk, trying to fix some things

--- ImagicNation/Assets/Scripts/EnemyScript.cs
+++ ImagicNation/Assets/Scripts/EnemyScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public GameObject self;   
+    public Rigidbody2D rb;
     public string type;
-    public GameObject target;
     public bool inRange = false;
+    public float attack = 0;
+    public float cd = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +19,33 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //slime script
         if(type == "slime"){
-            if(inRange){
-
+            if(inRange&&cd<=0&&attack<0){
+                attack = 0;
+            }else if(inRange&&cd<=0){
+                attack += Time.deltaTime;
+            }
+            if(attack>=0.66){
+                cd = 5;
+                rb.velocity = new Vector2(2, rb.velocity.y);
             }
         }
+        if(rb.velocity.x)
+        rb.velocity = new Vector2(rb.velocity.x-0.1f, rb.velocity.y);
     }
     public void onTriggerEnter(Collider col){
     
     }
     public void onTriggerStay(Collider col){
-        if(col.gameObject.name == target.name){
+        if(col.gameObject.name == "player"){
             inRange = true;
         }
     }
     public void onTriggerExit(Collider col){
-        inRange = false;
+        if(col.gameObject.name == "player"){
+            inRange = false;
+        }
     }
 }
