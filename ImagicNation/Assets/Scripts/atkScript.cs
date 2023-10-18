370734cb567056ef9f6600e7f19e82cf72bb974c
jamjammers <jamesetam@gmail.com>
2023-10-18 08:56:30 -0500

    camera, fireball
    camera: locate player, go to its position
    need to: make it have to be within bounds, raise a bit higher
    fireball: now will collide with the player after 0.1 seconds
    needto: explode anim

--- ImagicNation/Assets/Scripts/atkScript.cs
+++ ImagicNation/Assets/Scripts/atkScript.cs
@@ -7,6 +7,8 @@ public class atkScript : MonoBehaviour
     public string type;
     public int direction = 1;
     public float temp;
+    public bool start;
+    public float clock;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,27 @@ public class atkScript : MonoBehaviour
         this.transform.localScale = new Vector2(0.4f*direction,0.4f);
         
     }
-
     // Update is called once per frame
     void Update()
     {
        transform.Translate(Vector2.left*direction*Time.deltaTime*10);
- 
+       if(!start){
+        clock += Time.deltaTime;
+        if(clock>=0.1){
+            start = true;
+        }
+       }
     }
-    void OnCollisionStay2D(Collision2D col){
-        if(col.gameObject.name != "room"&&col.gameObject.name !="player"){
+    void OnCollisionEnter2D(Collision2D col){
+        if((col.gameObject.name != "room" && col.gameObject.name != "player")||(start&&col.gameObject.name == "player"))
+        {        
+            Debug.Log(col.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.name != "bounds"){
+            
             Debug.Log(col.gameObject.name);
             Destroy(gameObject);
         }
---
+++ ImagicNation/Assets/Scripts/camScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camScript : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cam.orthographicSize);
+        this.transform.position = player.transform.position+ new Vector3(0,0,-5);
+    }
+}
